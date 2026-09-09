0c5107656ac50832aff35c9d08edb88d4b21722e
TheGrowthConsultancy-01 <tgc.henil@gmail.com>
2026-09-09 23:57:14 +0530

    fix

--- frontend/src/components/CounsellingSpecializations.tsx
+++ frontend/src/components/CounsellingSpecializations.tsx
@@ -216,8 +216,7 @@
 
 
 import { Brain, Heart, GraduationCap, User, Users, Flame, Info, Calendar } from 'lucide-react';
-import React from 'react';
-import Navbar from './Navbar';
+
 
 interface CounsellingProps {
   onViewChange: (viewId: string) => void;
